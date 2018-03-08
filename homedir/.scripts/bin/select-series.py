49e2792176efbdb2ee1b916d4d40281923bc726b
Marcus Bruno <marcusbfs@gmail.com>
2018-03-08 11:30:00 -0300

    att python scripts

--- homedir/.scripts/bin/select-series.py
+++ homedir/.scripts/bin/select-series.py
@@ -9,27 +9,27 @@ vid_ext = ['.mp4', '.avi', '.mkv', '.flv', '.wmv', '.mov']
 
 def main():
 
-    mpath = "/run/media/mrcsbrn/mrcshd/Videos/Movies"
+    mpath = "/run/media/mrcsbrn/mrcshd/Videos/Series"
     cwd = mpath
 
     while has_dir_inside(cwd):
-        ctmp = dmenu_select_dir(cwd, '-p movies -l 30')
+        ctmp = dmenu_select_dir(cwd, '-p series -l 30')
         if not ctmp:
             return 1
         cwd = os.path.join(cwd, ctmp)
 
+    video = dmenu_select_video(cwd, '-p episodes -l 30')
+    video = os.path.join(cwd, video)
+
     # mpv_cmd = os.path.expandvars("command $PLAYER")
     mpv_cmd = 'mpv'
 
-    for f in os.listdir(cwd):
-        f_ext = os.path.splitext(f)[-1]
-        if f_ext in vid_ext:
-            subprocess.Popen(
-                mpv_cmd + ' ' + os.path.join(cwd, f),
-                shell=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL)
-            return 0
+    subprocess.Popen(
+        mpv_cmd + ' ' + video,
+        shell=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL)
+    return 0
 
 
 def has_dir_inside(path):
@@ -48,6 +48,26 @@ def dmenu_select_dir(path, args):
     ]
     if len(movies) == 1:
         return movies[0]
+
+    tmp = ''
+    for m in movies:
+        tmp += m + '\n'
+
+    _s = subprocess.run(
+        'echo "' + tmp.rstrip() + '" | sort -f | dmenu -i ' + args,
+        shell=True,
+        stdout=subprocess.PIPE).stdout
+    return str(_s, 'utf-8').rstrip()
+
+
+def dmenu_select_video(path, args):
+
+    movies = [
+        f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))
+        if os.path.splitext(os.path.join(path, f))[-1] in vid_ext
+    ]
+    if len(movies) == 1:
+        return movies
 
     tmp = ''
     for m in movies:
