a38c1b54beb3ba0af800b280c3989613cc5cce49
Marcus Bruno <marcusbfs@gmail.com>
2018-03-11 13:21:31 -0300

    dwm

--- data/suckless-tools/dwm/config.def.h
+++ data/suckless-tools/dwm/config.def.h
@@ -3,7 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx = 0; /* gap pixel between windows */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
@@ -11,10 +10,14 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int viewontag          = 1;     /* Switch view on tag switch */
-//static const char *fonts[]          = { "monospace:size=11" };
-//static const char dmenufont[]       = "monospace:size=11";
-static const char *fonts[]          = { "xos4 Terminus:style=Regular:size=11" };
-static const char dmenufont[]       = "xos4 Terminus:style=Regular:size=11";
+//static const char *fonts[]          = { "xos4 Terminus:style=Regular:size=11" };
+//static const char dmenufont[]       = "xos4 Terminus:style=Regular:size=11";
+//static const char *fonts[]          = { "Misc Termsyn:style=Regular:size=13" };
+//static const char dmenufont[]       = "Misc Termsyn:style=Regular:size=13";
+//#define SAME_FONT "Misc Termsyn:style=Regular:size=13"
+#define SAME_FONT "ProggyCleanTT CE:style=Regular:size=12"
+static const char *fonts[]          = { SAME_FONT };
+static const char dmenufont[]       = SAME_FONT;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -35,9 +38,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Vlc",      NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "mpv",      NULL,       NULL,       1 << 2,       1,           -1 },
+	{ "Firefox",      NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "qutebrowser",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Vlc",          NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "mpv",          NULL,       NULL,       1 << 2,       1,           -1 },
 };
 
 /* layout(s) */
@@ -45,10 +49,8 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
- 	{ "[\\]",      dwindle },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
@@ -61,6 +63,7 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/bash", "-c", cmd, NULL } }
 // https://github.com/cdown/dwm/blob/master/config.h
@@ -72,8 +75,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]  = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]   = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
 #include <X11/XF86keysym.h>
@@ -106,10 +109,9 @@ static Key keys[] = {
     {  MODKEY,            XK_h,      setmfact,       {.f = -0.05} },
     {  MODKEY,            XK_l,      setmfact,       {.f = +0.05} },
     // layout
-    {  MODKEY|ShiftMask,  XK_s,      setlayout,       {.v  =   &layouts[0]}  },
-    {  MODKEY,            XK_t,      setlayout,       {.v  =   &layouts[1]}  },
-    {  MODKEY,            XK_f,      setlayout,       {.v  =   &layouts[2]}  },
-    {  MODKEY,            XK_m,      setlayout,       {.v  =   &layouts[3]}  },
+    { MODKEY,             XK_t,      setlayout,      {.v = &layouts[0]} },
+    { MODKEY,             XK_f,      setlayout,      {.v = &layouts[1]} },
+    { MODKEY,             XK_m,      setlayout,      {.v = &layouts[2]} },
     {  MODKEY,            XK_space,  setlayout,       {0}  },
     {  MODKEY|ShiftMask,  XK_space,  togglefloating,  {0}  },
     // others
@@ -147,3 +149,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
