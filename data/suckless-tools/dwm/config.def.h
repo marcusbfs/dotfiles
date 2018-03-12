2a3b32af32df2164e688e07db76dc9ab31090265
Marcus Bruno <marcusbfs@gmail.com>
2018-03-12 19:08:01 -0300

    dwm att

--- data/suckless-tools/dwm/config.def.h
+++ data/suckless-tools/dwm/config.def.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int start_with_gap = 0;
+static const unsigned int start_with_gap = 1;
 static const unsigned int gap_px = 20; /* gap pixel between windows */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -12,11 +12,6 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int viewontag          = 1;     /* Switch view on tag switch */
-//static const char *fonts[]          = { "xos4 Terminus:style=Regular:size=11" };
-//static const char dmenufont[]       = "xos4 Terminus:style=Regular:size=11";
-//static const char *fonts[]          = { "Misc Termsyn:style=Regular:size=13" };
-//static const char dmenufont[]       = "Misc Termsyn:style=Regular:size=13";
-//#define SAME_FONT "Misc Termsyn:style=Regular:size=13"
 #define SAME_FONT "ProggyCleanTTSZ:style=Regular:size=12"
 static const char *fonts[]          = { SAME_FONT };
 static const char dmenufont[]       = SAME_FONT;
