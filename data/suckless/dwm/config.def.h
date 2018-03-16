6ed45f168695651405b6c5ac696e8d5a4122ff86
Marcus Bruno <marcusbfs@gmail.com>
2018-03-15 23:41:51 -0300

    dwm -> att colors

--- data/suckless/dwm/config.def.h
+++ data/suckless/dwm/config.def.h
@@ -15,17 +15,17 @@ static const int viewontag          = 1;     /* Switch view on tag switch */
 #define SAME_FONT "ProggyCleanTTSZ:style=Regular:size=12"
 static const char *fonts[]          = { SAME_FONT };
 static const char dmenufont[]       = SAME_FONT;
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#3c3c3c";
-static const char azul_bb[]         = "#00afff";
-static const char *colors[][3]      = {
+
+static const char norm_fg[]       = "#bbbbbb";
+static const char norm_bg[]       = "#222222";
+static const char norm_border[]   = "#444444";
+static const char sel_fg[]        = "#eeeeee";
+static const char sel_bg[]        = "#3c3c3c";
+static const char sel_border[]    = "#00afff";
+static const char *colors[][3]    = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  azul_bb},
+	[SchemeNorm] = { norm_fg,   norm_bg,   norm_border },
+	[SchemeSel]  = { sel_fg,    sel_bg,    sel_border},
 };
 
 /* tagging */
@@ -76,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
