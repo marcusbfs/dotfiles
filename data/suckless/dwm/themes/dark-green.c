c32ba864dd2b5521793adfaa7ea45addb9e2cbf9
Marcus Bruno <marcusbfs@gmail.com>
2018-03-18 15:51:13 -0300

    dwm -> themes

--- data/suckless/dwm/themes/dark-green.c
+++ data/suckless/dwm/themes/dark-green.c
@@ -1,15 +1,13 @@
-static const char norm_fg[] = "#cfdbcf";
-static const char norm_bg[] = "#153d2f";
+static const char norm_fg[]     = "#cfdbcf";
+static const char norm_bg[]     = "#153d2f";
 static const char norm_border[] = "#909990";
 
-static const char sel_fg[] = "#cfdbcf";
-static const char sel_bg[] = "#6D998B";
-//static const char sel_border[] = "#10c787";
-static const char sel_border[] = "#2ff1ad";
-
+static const char sel_fg[]      = "#cfdbcf";
+static const char sel_bg[]      = "#6D998B";
+static const char sel_border[]  = "#2ff1ad";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
     [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
-    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
+    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border  },  // the focused win
 };
--- data/suckless/dwm/themes/default.c
+++ data/suckless/dwm/themes/default.c
@@ -3,10 +3,10 @@ static const char norm_bg[]     = "#222222";
 static const char norm_border[] = "#444444";
 
 static const char sel_fg[]      = "#eeeeee";
-static const char sel_bg[]      = "#3c3c3c";
-static const char sel_border[]  = "#00afff";
+static const char sel_bg[]      = "#005577";
+static const char sel_border[]  = "#005577";
 
-static const char *colors[][3]    = {
+static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { norm_fg,   norm_bg,   norm_border },
 	[SchemeSel]  = { sel_fg,    sel_bg,    sel_border  },
