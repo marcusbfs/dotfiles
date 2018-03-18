80c05d0eb788f837ecf7570e1a0916caf181aa7a
Marcus Bruno <marcusbfs@gmail.com>
2018-03-18 14:33:37 -0300

    dwm -> themes

--- data/suckless/dwm/config.def.h
+++ data/suckless/dwm/config.def.h
@@ -16,17 +16,7 @@ static const int viewontag          = 1;     /* Switch view on tag switch */
 static const char *fonts[]          = { SAME_FONT };
 static const char dmenufont[]       = SAME_FONT;
 
-static const char norm_fg[]       = "#bbbbbb";
-static const char norm_bg[]       = "#222222";
-static const char norm_border[]   = "#444444";
-static const char sel_fg[]        = "#eeeeee";
-static const char sel_bg[]        = "#3c3c3c";
-static const char sel_border[]    = "#00afff";
-static const char *colors[][3]    = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { norm_fg,   norm_bg,   norm_border },
-	[SchemeSel]  = { sel_fg,    sel_bg,    sel_border},
-};
+#include "themes/selgray.c"
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
---
+++ data/suckless/dwm/themes/selgray.c
@@ -0,0 +1,11 @@
+static const char norm_fg[]       = "#bbbbbb";
+static const char norm_bg[]       = "#222222";
+static const char norm_border[]   = "#444444";
+static const char sel_fg[]        = "#eeeeee";
+static const char sel_bg[]        = "#3c3c3c";
+static const char sel_border[]    = "#00afff";
+static const char *colors[][3]    = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { norm_fg,   norm_bg,   norm_border },
+	[SchemeSel]  = { sel_fg,    sel_bg,    sel_border},
+};
